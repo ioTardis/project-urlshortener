7549762dd178f29085d593476dbaa4dc3252ffc2
Izabella Golovina <62325997+ioTardis@users.noreply.github.com>
2022-08-30 13:29:58 +0000

    initial commit edit README and index.html files

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # URL Shortener Microservice
 
-This is the boilerplate code for the URL Shortener Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/url-shortener-microservice.
+This is a Node.js (with Express.js) small app that uses MongoDB which is part of the FCC Back End Certification. 
+It listens POST requests with an URL in and sends back a JSON with original URL and shortened URL.
+If API recieve GET request with shortertened URL it will redirect user to the original URL
+
+## Example usage
+
+```
+https://project-urlshortener.iotardis.repl.co/api/shorturl/https://www.freecodecamp.org/
+https://project-urlshortener.iotardis.repl.co/api/shorturl/2
+```
+
+## Example output
+
+```
+{"original_url":"https://www.reddit.com/","short_url":6}
+```
+
+## [Try it](https://project-urlshortener.iotardis.repl.co)
--- index.js
+++ index.js
@@ -2,6 +2,22 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const regex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
+
+// Connection to the database
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Creating model
+let urlSchema = new mongoose.Schema({
+  original: {
+    type: String,
+    required: true
+  },
+  short: Number
+});
+let URL = mongoose.model('URL', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +26,64 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (req, res) => {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+// Reciving POST with original URL
+app.post('/api/shorturl', bodyParser.urlencoded({ extended: false }) , (req, res) => {
+  let originalURL = req.body.url;
+  // by default short URL is 1
+  let shortURL = 1;
+
+  // checking if URL is valid
+  if (!originalURL.match(regex)) {
+    res.json({ error: 'invalid url'});
+    return
+  }
+  
+  // searching the highest number of short URL to assign a new one
+  URL.findOne({})
+      .sort({short: 'desc'})
+      .exec((err, data) => {
+        // short URL is next number after biggest one in database
+        if (!err && data != undefined) {
+          shortURL = data.short + 1;
+        }
+        if (!err){
+          // adding or editing entry of original URL
+          URL.findOneAndUpdate(
+            { original: originalURL },
+            { original: originalURL, short: shortURL},
+            { new: true, upsert: true },
+            (err, savedURL) => {
+              if (!err){
+                shortURL = savedURL.short;
+                res.json({ original_url: originalURL, short_url: shortURL });
+              }
+            }
+          );
+        }
+  });
+});
+
+// Redirecting short URLs to original 
+app.get('/api/shorturl/:input', (req, res) => {
+  let input = req.params.input;
+  URL.findOne({ short: input }, (err, data) => {
+    if (!err && data != undefined){
+      res.redirect(data.original)
+    } else {
+      res.json('URL not Found');
+    }
+  });
 });
